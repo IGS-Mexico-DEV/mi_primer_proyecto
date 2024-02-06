0084cf9cbd56cd00577e4879fe1a84685b1989a2
César Valencia Campuzano <cesarvalencia@live.com.mx>
2024-02-06 17:55:35 -0600

    Se agrega nueva funcion pare envio de SMS con proveedor Bonsaif. ñalskjdfñlaksj dñlfkajsñlkefj qñowkejf ñalskdjf ñlaksjed sdñlacfkjsñldkfjqopiwef añlskdf +++++++++ +++++ +++++ alñskdjfñlaksjdfñlkajsdñlfakjsdñlfkja

---
+++ app/Http/Controllers/NuevaFuncionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NuevaFuncionController extends Controller {
+    function index()
+    {
+        return 'index';
+    }
+    function enviar()
+    {
+        return 'enviado';
+    }
+    function actualizar()
+    {
+        return 'actualizando';
+    }
+}
